d01f6f4893adb8938a8a00ae7e1bef0561597972
Derek Howard <derekhoward@gmail.com>
2018-05-04 15:12:46 -0400

    output a csv of cell type descriptions

--- linnarson_processing.R
+++ linnarson_processing.R
@@ -1,8 +1,10 @@
 library(readr)
 library(dplyr)
 library(magrittr)
+getwd()
 
 table <- read_tsv('./l5_all.agg.tab', col_names = FALSE)
+descriptions <- table[29, 9:273]
 
 table %<>% filter(!is.na(X1) | X8 == 'ClusterName')
 colnames(table) <- c(unlist(table[2,1:7]), unlist(table[1, 8:ncol(table)]))
@@ -10,15 +12,17 @@ table <- table[-c(1,2),]
 
 # columns to drop
 dropcols <- c('Accession', '_LogCV', '_LogMean', '_Selected', '_Total', '_Valid', 'ClusterName', NA)
-
 table %<>% select(-one_of(dropcols))
 
 #to change all but the 'Gene' column name into numeric type
 table[,2:length(colnames(table))] %<>% lapply(function(x) as.numeric(as.character(x)))
 
+cell_type_info <- rbind(colnames(table[2:266]), descriptions)
+cell_type_info <- t(cell_type_info)
+colnames(cell_type_info) <- c('cell_type', 'description')
+write.csv(cell_type_info, file = "celltype_descriptions.csv", row.names=FALSE)
 #now melt
 linnarsson <- as_tibble(reshape2::melt(table, id.vars='Gene', variable.name='cluster_id', value.name='expression'))
-
 linnarsson %<>% mutate(log1Expression=log(1+expression))
 
 # zscore across genes
